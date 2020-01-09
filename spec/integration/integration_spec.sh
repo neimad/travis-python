300401a1bb95d30263b345303bc9f9274463cfd8
Damien Flament <damien.flament@gmx.com>
2020-01-09 17:27:57 +0100

    Enable integration tests on CI.

--- spec/integration/integration_spec.sh
+++ spec/integration/integration_spec.sh
@@ -1,5 +1,7 @@
 #shellcheck shell=bash
 
+Include ./travis-python.bash
+
 Context "When on Travis CI"
     TRAVIS=${TRAVIS:-}
     Skip if "not on Travis CI" test "$TRAVIS" != "true"
@@ -8,19 +10,15 @@ Context "When on Travis CI"
     After 'cleanup_directory'
     directory=${directory:-}
 
-    Describe "travis-python"
+    Describe "install_python()"
         PYTHON=${PYTHON:-}
         Skip if "Python version is not specified" test -z "$PYTHON"
 
         It "installs Python $PYTHON to specified directory"
-            When call ./travis-python "$directory/Python" "$PYTHON"
-            The line 1 of output should start with "travis-python"
-            The line 2 of output should equal "Python tools for Travis CI."
-            The line 3 of output should equal "Installing latest Pyenv to $directory/Python..."
-            The line 4 of output should start with "Installed Pyenv"
-            The line 5 of output should start with "Installing Python"
-            The line 6 of output should start with "Installed Python $PYTHON"
-            The line 7 of output should be blank
+            When call install_python "$directory/Python" "$PYTHON"
+            The line 1 of output should start with "Installing Python"
+            The line 2 of output should start with "Installed Python $PYTHON"
+            The line 3 of output should be blank
             The result of function "python --version" should start with "Python $PYTHON"
             The result of function "which python" should equal "$directory/Python/shims/python"
         End
