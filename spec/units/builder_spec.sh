fed924aeb28a3f7f472ae94c2a824fccb12aa8e5
Damien Flament <damien.flament@gmx.com>
2020-04-29 23:40:47 +0200

    Be stricter in string expectations.

--- spec/units/builder_spec.sh
+++ spec/units/builder_spec.sh
@@ -27,12 +27,12 @@ Describe "__install_builder()"
         When call __install_builder "$directory"
         The command "__update_git_repo https://github.com/pyenv/pyenv /tmp/pyenv" should be called
         The command "/tmp/pyenv/plugins/python-build/install.sh" should be called
-        The line 1 of output should be blank
-        The line 2 of output should equal "> Installing python-build..."
-        The line 3 of output should equal "  requested location: $directory"
-        The line 4 of output should equal "  installed version: 20200110"
-        The line 5 of output should equal "  Done."
-        The lines of output should equal 5
+        The line 1 of entire output should be blank
+        The line 2 of entire output should equal "> Installing python-build..."
+        The line 3 of entire output should equal "  requested location: $directory"
+        The line 4 of entire output should equal "  installed version: 20200110"
+        The line 5 of entire output should equal "  Done."
+        The lines of entire output should equal 5
         The variable PATH should start with "$directory/bin:"
     End
 End
@@ -59,15 +59,15 @@ Describe "__available_python_versions_from_builder()"
             '
 
         When call __available_python_versions_from_builder
-        The line 1 of output should equal "2.3.1"
-        The line 2 of output should equal "3.7.6"
-        The line 3 of output should equal "3.7.7"
-        The line 4 of output should equal "3.8.0"
-        The line 5 of output should equal "3.8.0-dev"
-        The line 6 of output should equal "3.8.1"
-        The line 7 of output should equal "activepython-3.7.0"
-        The line 8 of output should equal "anaconda-4.0.0"
-        The lines of output should equal 8
+        The line 1 of entire output should equal "2.3.1"
+        The line 2 of entire output should equal "3.7.6"
+        The line 3 of entire output should equal "3.7.7"
+        The line 4 of entire output should equal "3.8.0"
+        The line 5 of entire output should equal "3.8.0-dev"
+        The line 6 of entire output should equal "3.8.1"
+        The line 7 of entire output should equal "activepython-3.7.0"
+        The line 8 of entire output should equal "anaconda-4.0.0"
+        The lines of entire output should equal 8
     End
 
     It "gives a blank output if no version are available"
